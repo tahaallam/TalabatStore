881f8da965450f588b79c9e9850d8a2cfdb8a0fe
Taha <Taha@DESKTOP-SGO0H8A>
2024-10-15 06:45:58 +0300

    Creating New Project

--- Talabat.APIs/WeatherForecast.cs
+++ Talabat.APIs/WeatherForecast.cs
@@ -5,9 +5,9 @@ namespace Talabat.APIs
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+          
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; set; }  
     }
 }
