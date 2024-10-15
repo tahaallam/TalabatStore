4f67484598789f2ff1e817855e69667a3979bafb
Taha <Taha@DESKTOP-SGO0H8A>
2024-10-15 17:10:23 +0300

    Generic Repository Product Module

---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T :BaseEntity 
+    {
+        private readonly StoreContext _dbcontext;
+
+        public GenericRepository(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbcontext.Set<T>().ToListAsync();    
+        }
+
+        public async Task<T> GetById(int id)
+        {
+           return await _dbcontext.Set<T>().FindAsync(id);
+        }
+    }
+}
