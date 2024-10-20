0d1028765c58b1f3101686eb0215822e069f3b3b
Taha <Taha@DESKTOP-SGO0H8A>
2024-10-20 12:11:48 +0300

    Cleaning Up Program Class

---
+++ Talabat.APIs/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Errors;
+using Talabat.APIs.Helper;
+using Talabat.Core.Repositories;
+using Talabat.Repository;
+
+namespace Talabat.APIs.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection Services)
+        {
+            // builder.Services.AddScoped<IGenericRepository<Product>, GenericRepository<Product>>();
+            Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            Services.AddAutoMapper(typeof(MappingProfile));
+            Services.Configure<ApiBehaviorOptions>(Options => {
+                Options.InvalidModelStateResponseFactory = (actioncontext) =>
+                {
+                    var errors = actioncontext.ModelState.Where(P => P.Value.Errors.Count() > 0)
+                                                        .SelectMany(P => P.Value.Errors)
+                                                        .Select(E => E.ErrorMessage)
+                                                        .ToArray();
+                    var ValidationErrorResponse = new ApiValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(ValidationErrorResponse);
+                };
+            });
+            return Services;
+        }
+    }
+}
