3a303f3b331227214fa3d98817880ddefb2a560f
Taha <Taha@DESKTOP-SGO0H8A>
2024-10-25 14:54:41 +0300

    BasketModulEntity

---
+++ Talabat.Core/Entities/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public List<BasketItem> BasketItems { get; set; }
+    }
+}
