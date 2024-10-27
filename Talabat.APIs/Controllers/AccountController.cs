9e90f0bd581e9f33223404651af0dd3e673d8b4f
Taha <Taha@DESKTOP-SGO0H8A>
2024-10-27 18:13:46 +0300

    AccountComtroller - Register EndPoint

---
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.DTOs;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.APIs.Controllers
+{
+   
+    public class AccountController : ApiBaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            var User = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split('@')[0],
+                PhoneNumber = model.PhoneNumber,
+            };
+          var result =  await _userManager.CreateAsync(User , model.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+            var ReturnedUser = new UserDto()
+            {
+                DisplayName = User.DisplayName ,
+                Email = User.Email ,
+                Token = "this will be token"
+            };
+            return Ok(ReturnedUser);
+          
+        }
+    }
+}
